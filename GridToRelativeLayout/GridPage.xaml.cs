6f719524a67d98136fab2f16cf2565bee0b0221c
brminnick <brandon.r.minnick@gmail.com>
2017-08-23 14:12:12 -0700

    Finished RelativeLayoutPage

--- GridToRelativeLayout/GridPage.xaml.cs
+++ GridToRelativeLayout/GridPage.xaml.cs
@@ -7,6 +7,27 @@ namespace GridToRelativeLayout
         public GridPage()
         {
             InitializeComponent();
+
+            SetBackgroundColors(true);
+        }
+
+        void SetBackgroundColors(bool areBackgroundColorsEnabled)
+        {
+            switch(areBackgroundColorsEnabled)
+            {
+                case false:
+                    return;
+            }
+
+            emptyGrid.BackgroundColor = Color.Aqua;
+            phraseGrid.BackgroundColor = Color.Green;
+            prGrid.BackgroundColor = Color.Gray;
+            siGrid.BackgroundColor = Color.Yellow;
+            wordGrid.BackgroundColor = Color.Blue;
+            textLabelGrid.BackgroundColor = Color.Brown;
+            detailGrid.BackgroundColor = Color.Lime;
+            buttonGrid.BackgroundColor = Color.Red;
+            tapGrid.BackgroundColor = Color.Teal;
         }
     }
 }
--- GridToRelativeLayout/RelativeLayoutPage.xaml.cs
+++ GridToRelativeLayout/RelativeLayoutPage.xaml.cs
@@ -51,20 +51,33 @@ namespace GridToRelativeLayout
                 Margin = new Thickness(0, 26, 0, 0)
             };
 
-            var faveLabel = new Label { Text = "Fave" };
-            var wordTypeLabel = new Label { Text = "Word" };
+            var faveLabel = new Label { Text = "Fave Label" };
+            var wordTypeLabel = new Label { Text = "Word Type Label" };
 
             var textLabel = new Label
             {
+                BackgroundColor = Color.Brown,
                 Text = "Text Label",
                 FontSize = 45,
                 HorizontalTextAlignment = TextAlignment.Center,
                 VerticalTextAlignment = TextAlignment.Center
             };
 
-            var detail1Label = new Label { Text = "detail1" };
-            var detail2Label = new Label { Text = "detail2" };
-            var detail3Label = new Label { Text = "detail3" };
+            var detail1Label = new Label
+            {
+                Text = "Detail 1 Label",
+                Margin = new Thickness(10, 0)
+            };
+            var detail2Label = new Label
+            {
+                Text = "Detail 2 Label",
+                Margin = new Thickness(10, 0)
+            };
+            var detail3Label = new Label
+            {
+                Text = "Detail 3 Label",
+                Margin = new Thickness(10, 0)
+            };
 
             var zeroButton = new Button
             {
@@ -90,8 +103,15 @@ namespace GridToRelativeLayout
             var tapScreenLabel = new Label
             {
                 Text = "Tap Screen",
-                Margin = new Thickness(0, 0, 0, 20)
+                Margin = new Thickness(0, 0, 0, 20),
+                VerticalTextAlignment = TextAlignment.Center,
+                VerticalOptions = LayoutOptions.Center
             };
+
+            Func<RelativeLayout, double> GetZeroButtonHeight = parent => zeroButton.Measure(parent.Width, parent.Height).Request.Height;
+            Func<RelativeLayout, double> GetOneButtonHeight = parent => oneButton.Measure(parent.Width, parent.Height).Request.Height;
+            Func<RelativeLayout, double> GetTwoButtonHeight = parent => twoButton.Measure(parent.Width, parent.Height).Request.Height;
+            Func<RelativeLayout, double> GetFiveButtonHeight = parent => fiveButton.Measure(parent.Width, parent.Height).Request.Height;
 
             var relativeLayout = new RelativeLayout();
             relativeLayout.Children.Add(emptyLabel,
@@ -122,7 +142,40 @@ namespace GridToRelativeLayout
                                         Constraint.RelativeToView(faveLabel, (parent, view) => view.X + view.Width),
                                         Constraint.RelativeToView(msg1Label, (parent, view) => view.Y + view.Height + 20),
                                         Constraint.RelativeToParent(parent => parent.Width * 0.25));
-
+            relativeLayout.Children.Add(textLabel,
+                                        Constraint.Constant(20),
+                                        Constraint.RelativeToView(faveLabel, (parent, view) => view.Y + view.Height + 20),
+                                        Constraint.RelativeToParent(parent => parent.Width - 40),
+                                        Constraint.RelativeToParent(parent => parent.Height * 0.25));
+            relativeLayout.Children.Add(detail1Label,
+                                       Constraint.Constant(20),
+                                       Constraint.RelativeToView(textLabel, (parent, view) => view.Y + view.Height + 20));
+            relativeLayout.Children.Add(detail2Label,
+                                       Constraint.Constant(20),
+                                       Constraint.RelativeToView(detail1Label, (parent, view) => view.Y + view.Height));
+			relativeLayout.Children.Add(detail3Label,
+									   Constraint.Constant(20),
+									   Constraint.RelativeToView(detail2Label, (parent, view) => view.Y + view.Height));
+            relativeLayout.Children.Add(zeroButton,
+                                        Constraint.Constant(40),
+                                        Constraint.RelativeToParent(parent => parent.Height - GetZeroButtonHeight(parent) - 40),
+                                        Constraint.RelativeToParent(parent => (parent.Width - 80) / 4));
+            relativeLayout.Children.Add(oneButton,
+                                        Constraint.RelativeToView(zeroButton, (parent, view) => view.X + view.Width),
+                                        Constraint.RelativeToParent(parent => parent.Height - GetZeroButtonHeight(parent) - 40),
+                                        Constraint.RelativeToParent(parent => (parent.Width - 80) / 4));
+            relativeLayout.Children.Add(twoButton,
+                                        Constraint.RelativeToView(oneButton, (parent, view) => view.X + view.Width),
+                                        Constraint.RelativeToParent(parent => parent.Height - GetZeroButtonHeight(parent) - 40),
+                                        Constraint.RelativeToParent(parent => (parent.Width - 80) / 4));
+            relativeLayout.Children.Add(fiveButton,
+                                        Constraint.RelativeToView(twoButton, (parent, view) => view.X + view.Width),
+                                        Constraint.RelativeToParent(parent => parent.Height - GetZeroButtonHeight(parent) - 40),
+                                        Constraint.RelativeToParent(parent => (parent.Width - 80) / 4));
+            relativeLayout.Children.Add(tapScreenLabel,
+                                        Constraint.Constant(20),
+                                        Constraint.RelativeToView(zeroButton, (parent, view) => view.Y),
+                                        Constraint.RelativeToParent(parent => parent.Width - 40));
 
             Padding = GetPadding();
             Content = relativeLayout;
